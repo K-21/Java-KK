97f48ba5b508b7d7126e6992be1915e141542b43
Kritika <jhoothisahi@gmail.com>
2024-04-30 23:27:28 +0530

    Leap year

---
+++ 01-first-flow-program/leapYear.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class leapYear {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the year: ");
+        int n=sc.nextInt();
+        if(n%4==0 && n%100!=0 ){
+            System.out.println("Year  " +n +" is leap year ");
+        }
+        else if (n%400==0) 
+        System.out.println("Year   "+n+" is leap year");
+        else
+        System.out.println("Year   "+n+" not leap year ");
+
+        sc.close();
+    }
+    
+}
