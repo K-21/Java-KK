e6e60bc0cb2f98d22b954041f1601e7b8c4d3892
Kritika <jhoothisahi@gmail.com>
2024-04-30 23:28:26 +0530

    Sum of  2 Numbers

---
+++ 01-first-flow-program/sumofTwo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sumofTwo {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the 1st number:");
+        int a=sc.nextInt();
+        System.out.println("Enter the 2nd number:");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("The sum of two numbers is:"+sum);
+        sc.close();
+    }
+}
