25ce6328f49b521db78cf4ac50201cdc1c3f1d31
Kritika <jhoothisahi@gmail.com>
2024-04-30 23:27:15 +0530

    HCF

---
+++ 01-first-flow-program/HCF.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int hcf=1;
+        for(int i=1;i<=a||i<=b;i++){
+            if(a%i==0&&b%i==0){
+                hcf=i;
+            }
+        }
+        System.out.println("HCF of "+a+" and "+b+" is "+hcf);
+               s.close();
+    }
+//        s.close();
+    }
+    
+
