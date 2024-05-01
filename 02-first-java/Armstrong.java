5ef4d8cc0913e7aacb6a12373bd5474dcf469250
K-21 <84450912+K-21@users.noreply.github.com>
2024-05-01 06:47:16 +0530

    Update Armstrong.java

--- 02-first-java/Armstrong.java
+++ 02-first-java/Armstrong.java
@@ -61,6 +61,34 @@ for (int number=100;number<=1000;number++){
   }
   
 }
+    // To check Armstrong between two given numbers
+    System.out.println("Enter the staring range of the number");
+        int n1=sc.nextInt();
+        System.out.println("Enter the last range of the number");
+        int n2=sc.nextInt();
+        
+        if(n1>n2){
+            System.out.println("range not specified correctly");
+            System.exit(n1);
+            }
+        while (n1<=n2){
+       int temp2=n1;
+      int lastdigit=temp2%10;
+      temp2/=10;
+    
+      int secondDigit=temp2%10;
+      temp2/=10;
+    
+      int thirdDigit=temp2%10;
+      temp2/=10;
+    
+      int sumC=(lastdigit*lastdigit*lastdigit)+(secondDigit*secondDigit*secondDigit)+(thirdDigit*thirdDigit*thirdDigit);
+     // System.out.println(sumC);
+      if (sumC==n1){
+        System.out.println("Armstrong number:"+sumC);
+      }
+      n1++;
+    }
 }
 }
 
